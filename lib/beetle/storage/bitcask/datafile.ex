3ec66bfdf8b5119865b850ca581d383fe80f02be
prxssh <prxssh@pm.me>
2025-01-05 22:49:27 +0530

    feat: start implementing datafiles

--- lib/beetle/storage/bitcask/datafile.ex
+++ lib/beetle/storage/bitcask/datafile.ex
@@ -1,3 +1,45 @@
 defmodule Beetle.Storage.Bitcask.Datafile do
-  @moduledoc false
+  @moduledoc """
+  Datafile is an append-only log file that holds the key-value pair along with
+  some metadata. 
+
+  A single Bitcask instance could have many datafiles, out of which only one
+  will be active and opened for writing, while the other are considered
+  immutable and are only used for reads. When the active datafile meets a size
+  threshold, it is closed and a new active datafile is created.
+  """
+end
+
+defmodule Beetle.Storage.Bitcask.Datafile.Entry do
+  @moduledoc """
+  Represents an entry in a Bitcask Datafile.
+
+  Each entry in the datafile has the following format: 
+
+     --------------------------------------------------------------------
+    | crc | expiration | key_size | value_size | key | serialized_value |
+    --------------------------------------------------------------------
+
+  - `crc`: CRC32 hash of the entry (expiration + key size + value size + key + value)
+  - `expiration`: unsigned integer representing the TTL for the key (0 for no expiration)
+  - `key_size`: size of key in bytes
+  - `value_size`: size of value in bytes
+  - `serialized_value`: value serialized using `:erlang.term_to_binary/1`
+
+  All integers are stored in big-endian format. The CRC is calculated over all
+  the fields that follow it in the entry.
+
+  Values can be any Erlang/Elixir term (lists, maps, sets, tuples, etc) as they
+  are automatically serialized before storage and deserialized upon retrieval.
+  """
+  @type t :: %__MODULE__{
+          crc: pos_integer(),
+          expiration: non_neg_integer(),
+          key_size: pos_integer(),
+          value_size: pos_integer(),
+          key: String.t(),
+          value: binary()
+        }
+
+  defstruct [:crc, :expiration, :key_size, :value_size, :value]
 end
