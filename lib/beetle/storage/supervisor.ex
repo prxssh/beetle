0f3b2956d683ce4a939e1f066922c768540099f7
prxssh <prxssh@pm.me>
2025-01-11 23:18:12 +0530

    feat: shard supervisor

---
+++ lib/beetle/storage/supervisor.ex
@@ -0,0 +1,36 @@
+defmodule Beetle.Storage.Supervisor do
+  @moduledoc """
+  Supervises multiple Bitcask storage engine, each handling a shard of the
+  keyspace. Provides automatic distribution of keys across shards using
+  consistent hashing.
+  """
+  use Supervisor
+
+  alias Beetle.Config
+
+  # ==== Client
+
+  def start_link(_), do: Supervisor.start_link(__MODULE__, nil, name: __MODULE__)
+
+  # ==== Server
+
+  @impl true
+  def init(_) do
+    shards = Config.database_shards()
+
+    children =
+      [
+        {Registry, keys: :unique, name: Beetle.ShardRegistry},
+        for shard_id <- 0..(shards - 1) do
+          %{
+            restart: :permanent,
+            id: {:storage_engine, shard_id},
+            start: {Beetle.Storage.Engine, :start_link, [shard_id]}
+          }
+        end
+      ]
+      |> List.flatten()
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
