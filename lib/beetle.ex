8a2839d7b05bb6aeb50b49111473399911dcc01a
prxssh <prxssh@pm.me>
2025-04-09 16:46:11 +0530

    chore: reinit the project

--- lib/beetle.ex
+++ lib/beetle.ex
@@ -1,3 +1,18 @@
 defmodule Beetle do
-  @moduledoc false
+  @moduledoc """
+  Documentation for `Beetle`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Beetle.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
 end
